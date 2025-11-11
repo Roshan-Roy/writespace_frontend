86b7dfa5684e858581f385f2149dc59061929c5f
Roshan-Roy <roshanroy2911@gmail.com>
2025-11-11 23:29:04 +0530

    designing home page

--- src/components/mycomponents/navbar/Navbar.jsx
+++ src/components/mycomponents/navbar/Navbar.jsx
@@ -1,6 +1,13 @@
+import { Button } from "@/components/ui/button"
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="flex justify-center border-b border-b-foreground">
+      <div className="w-17/20 flex justify-between items-center h-20 gap-2">
+        <span className="font-logo text-2xl">Writespace</span>
+        <Button className="rounded-full text-xs">Get started</Button>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,15 @@ import { Button } from "@/components/ui/button"
 
 const Home = () => {
     return (
-        <div>
-            <p>Helo</p>
-            <Button>Click me</Button>
+        <div className="flex flex-col items-center h-[calc(100dvh-80px)]">
+            <div className="flex-1 flex flex-col justify-center w-17/20 gap-2">
+                <h1 className="font-heading text-[52px] leading-tight">Open<br />minds <span className="whitespace-nowrap">& words</span></h1>
+                <h2 className="text-xl">Write, read, and share stories that live beyond the page</h2>
+                <Button className="self-start mt-4 rounded-full bg-[#1A8917]" size="lg">Start Reading</Button>
+            </div>
+            <div className="border border-t-foreground w-full h-15">
+                hello
+            </div>
         </div>
     )
 }
