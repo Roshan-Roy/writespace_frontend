d9081e47ad55ea120e64a5bee8b98e9642c96846
Roshan-Roy <roshanroy2911@gmail.com>
2025-11-12 23:04:08 +0530

    homepage design completed and made responsive 12-11-2025

--- src/components/mycomponents/navbar/Navbar.jsx
+++ src/components/mycomponents/navbar/Navbar.jsx
@@ -2,10 +2,10 @@ import { Button } from "@/components/ui/button"
 
 const Navbar = () => {
   return (
-    <div className="flex justify-center border-b border-b-foreground">
+    <div className="flex justify-center border-b border-b-foreground bg-background-home">
       <div className="w-17/20 flex justify-between items-center h-20 gap-2">
-        <span className="font-logo text-2xl">Writespace</span>
-        <Button className="rounded-full text-xs">Get started</Button>
+        <span className="font-logo text-2xl lg:text-3xl">Writespace</span>
+        <Button className="rounded-full text-xs lg:text-sm lg:h-10 lg:px-6">Get started</Button>
       </div>
     </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,20 @@
+import { Info, Sun } from "lucide-react"
+import { Link } from "react-router"
 import { Button } from "@/components/ui/button"
 
 const Home = () => {
     return (
-        <div className="flex flex-col items-center h-[calc(100dvh-80px)]">
-            <div className="flex-1 flex flex-col justify-center w-17/20 gap-3">
-                <h1 className="font-heading text-[52px] leading-tight">Open<br />minds <span className="whitespace-nowrap">& words</span></h1>
-                <h2 className="text-xl">Write, read, and share stories that live beyond the page</h2>
-                <Button className="self-start mt-5 rounded-full bg-[#1A8917]" size="lg">Start Reading</Button>
+        <div className="flex flex-col items-center h-[calc(100dvh-80px)] bg-background-home">
+            <div className="flex-1 flex flex-col justify-center w-17/20 gap-3 lg:gap-5 sm:text-center">
+                <h1 className="font-heading text-[56px] sm:text-[70px] lg:text-[90px] leading-tight">Open<br />minds <span className="whitespace-nowrap">& words</span></h1>
+                <h2 className="text-xl lg:text-2xl">Write, read, and share stories that live beyond the page</h2>
+                <Button className="self-start sm:self-center mt-5 rounded-full bg-[#1A8917] lg:px-10 lg:h-12 lg:text-base" size="lg">Start Reading</Button>
             </div>
-            <div className="border border-t-foreground w-full h-15">
-                hello
+            <div className="border-t border-t-foreground w-full flex justify-center">
+                <div className="h-15 lg:h-17 w-17/20 flex items-center sm:justify-center gap-3 lg:gap-4">
+                    <Link href="/login"><Info className="lg:w-7 lg:h-7"/></Link>
+                    <Sun className="cursor-pointer lg:w-7 lg:h-7"/>
+                </div>
             </div>
         </div>
     )
