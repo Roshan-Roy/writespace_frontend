ca6d90f625d15629d2b8566a7d89a06440645ee0
Roshan-Roy <roshanroy2911@gmail.com>
2025-11-27 23:33:47 +0530

    auth modals design completed

--- src/components/modals/authmodal/AuthModal.jsx
+++ src/components/modals/authmodal/AuthModal.jsx
@@ -3,7 +3,6 @@ import SignInForm from "./SignInForm"
 import SignUpForm from "./SignUpForm"
 
 export function AuthModal({ openModal, setOpenModal }) {
-
     return (
         <Dialog open={!!openModal} onOpenChange={() => setOpenModal(null)}>
             <DialogContent className="max-w-full sm:max-w-sm h-dvh sm:h-auto flex flex-col justify-center p-8 sm:p-9 border-0 sm:border rounded-none sm:rounded-lg">
