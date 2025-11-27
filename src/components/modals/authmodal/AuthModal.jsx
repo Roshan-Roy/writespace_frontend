ca6797170bdc837ff01da306efc0ea2d9a1dea49
Roshan-Roy <roshanroy2911@gmail.com>
2025-11-28 00:22:51 +0530

    auth modals design completed

--- src/components/modals/authmodal/AuthModal.jsx
+++ src/components/modals/authmodal/AuthModal.jsx
@@ -5,7 +5,7 @@ import SignUpForm from "./SignUpForm"
 export function AuthModal({ openModal, setOpenModal }) {
     return (
         <Dialog open={!!openModal} onOpenChange={() => setOpenModal(null)}>
-            <DialogContent className="max-w-full sm:max-w-sm h-dvh sm:h-auto flex flex-col justify-center p-8 sm:p-9 border-0 sm:border rounded-none sm:rounded-lg">
+            <DialogContent className="max-w-full sm:max-w-sm h-dvh sm:h-auto flex flex-col justify-center p-8 sm:p-9 border-0 sm:border rounded-none sm:rounded-lg" onOpenAutoFocus={e => e.preventDefault()}>
                 {openModal === "signup" && <SignUpForm setOpenModal={setOpenModal} />}
                 {openModal === "signin" && <SignInForm setOpenModal={setOpenModal} />}
             </DialogContent>
