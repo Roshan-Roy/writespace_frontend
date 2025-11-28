22b910ad1ccb389f5ad36e28b53115545c2dd6cf
Roshan-Roy <roshanroy2911@gmail.com>
2025-11-28 23:14:38 +0530

    validation with zod on sign up form

--- src/components/modals/authmodal/SignUpForm.jsx
+++ src/components/modals/authmodal/SignUpForm.jsx
@@ -3,28 +3,78 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import InputError from "./InputError"
 import { Button } from "@/components/ui/button"
+import { useState } from "react"
+import * as z from "zod"
+
+const minError = (field, n) => `${field} must be at least ${n} characters long`;
+const maxError = (field, n) => `${field} cannot exceed ${n} characters`;
+const formatErrors = (errors) => {
+    return errors.reduce((acc, issue) => {
+        const field = issue.path[0];
+        acc[field] = issue.message;
+        return acc;
+    }, {});
+}
+const signupSchema = z.object({
+    username: z
+        .string()
+        .min(3, minError("Username", 3))
+        .max(20, maxError("Username", 20))
+        .trim(),
+    email: z.email("Please enter a valid email address"),
+    password: z
+        .string()
+        .min(8, minError("Password", 8))
+        .max(32, maxError("Password", 32)),
+});
+
 
 const SignUpForm = ({ setOpenModal }) => {
+    const [data, setData] = useState({
+        username: "",
+        email: "",
+        password: ""
+    })
+    const [errors, setErrors] = useState({
+        username: "",
+        email: "",
+        password: ""
+    })
+
     const handleFormChange = () => {
         setOpenModal("signin")
     }
+    const handleFormSubmit = (e) => {
+        e.preventDefault()
+
+        const result = signupSchema.safeParse(data);
+        if (result.success) {
+            console.log('Valid user data:', result.data);
+        } else {
+            console.error('Validation errors:', formatErrors(result.error.issues));
+        }
+    }
+    const handleInputChange = (value, fieldName) => {
+        setData(prevData => ({ ...prevData, [fieldName]: value }))
+    }
+
     return (
-        <form className="flex flex-col gap-8">
+        <form className="flex flex-col gap-8" onSubmit={handleFormSubmit}>
             <DialogTitle className="text-3xl text-center font-heading">Sign up</DialogTitle>
             <div>
                 <div className="flex flex-col gap-2 w-full">
                     <Label htmlFor="username">Username</Label>
-                    <Input id="username" name="username" placeholder="Enter username" className="h-11 px-4 rounded-2xl" />
+                    <Input id="username" placeholder="Enter username" className="h-11 px-4 rounded-2xl" value={data.username} onChange={e => handleInputChange(e.target.value, "username")} />
                 </div>
                 <InputError />
                 <div className="flex flex-col gap-2">
                     <Label htmlFor="email">Email</Label>
-                    <Input id="email" name="email" placeholder="Enter email" className="h-11 px-4 rounded-2xl" />
+                    <Input id="email" placeholder="Enter email" className="h-11 px-4 rounded-2xl" value={data.email} onChange={e => handleInputChange(e.target.value, "email")} />
                 </div>
                 <InputError />
                 <div className="flex flex-col gap-2">
                     <Label htmlFor="password">Password</Label>
-                    <Input id="password" name="password" type="password" placeholder="Enter password" className="h-11 px-4 rounded-2xl" aria-invalid={false} />
+                    <Input id="password" type="password" placeholder="Enter password" className="h-11 px-4 rounded-2xl" value={data.password} onChange={e => handleInputChange(e.target.value, "password")} />
                 </div>
                 <InputError />
                 <Button className="w-full mt-3 h-11 rounded-2xl">Sign Up</Button>
