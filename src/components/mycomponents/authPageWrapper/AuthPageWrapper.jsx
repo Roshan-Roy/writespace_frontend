5a7a9e6f2343fa4b8ae6c1a9bb3b6f243ef6b504
Roshan-Roy <roshanroy2911@gmail.com>
2025-12-04 18:34:32 +0530

    basic designs completed

--- src/components/mycomponents/authPageWrapper/AuthPageWrapper.jsx
+++ src/components/mycomponents/authPageWrapper/AuthPageWrapper.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AuthPageWrapper = ({ children }) => {
     return (
-        <div className="w-full h-dvh flex justify-center items-center">
+        <div className="w-full h-dvh flex justify-center items-center min-h-[550px]">
             <div className="flex flex-col items-center gap-4 sm:gap-6 w-full">
                 <h1 className="font-logo text-4xl sm:text-5xl">Writespace</h1>
                 {children}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
 
     return (
         <>
-            <div className="flex flex-col items-center h-[calc(100dvh-80px)] bg-background-home">
+            <div className="flex flex-col items-center h-[calc(100dvh-80px)] bg-background-home min-h-[450px]">
                 <div className="flex-1 flex flex-col justify-center w-17/20 gap-3 lg:gap-5 sm:text-center">
                     <h1 className="font-heading text-[54px] sm:text-[70px] lg:text-[90px] leading-tight">Open<br />minds <span className="whitespace-nowrap">& words</span></h1>
                     <h2 className="text-xl lg:text-2xl">Write, read, and share stories that live beyond the page</h2>
