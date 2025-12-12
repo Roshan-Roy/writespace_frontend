7789a8d6e1247ee00e5df5ce8b65bd6be5e30053
Roshan-Roy <roshanroy2911@gmail.com>
2025-12-12 14:00:43 +0530

    mask email with signout button and toggle theme

--- src/components/mycomponents/signoutButton/SignoutButton.jsx
+++ src/components/mycomponents/signoutButton/SignoutButton.jsx
@@ -2,7 +2,7 @@ import maskEmail from "@/lib/maskEmail"
 
 const SignoutButton = () => {
     return (
-        <div className="flex flex-col select-none gap-1.5 px-6 pt-6 pb-8 text-foreground/80 hover:text-foreground">
+        <div className="flex flex-col select-none cursor-pointer gap-1.5 px-6 pt-6 pb-8 text-foreground/80 hover:text-foreground">
             <span>Sign out</span>
             <span className="text-xs truncate">{maskEmail("roshanroy2911@gmail.com")}</span>
         </div>
