859a6018a5c32a5cf5bad5eee186f367acb9a628
Roshan-Roy <roshanroy2911@gmail.com>
2025-12-29 16:06:23 +0530

    first page of write design finished

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -19,6 +19,7 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        success: "bg-green-btn text-green-btn-foreground hover:bg-green-btn/90"
       },
       size: {
         default: "h-9 px-5 py-2 has-[>svg]:px-3",
--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,25 +1,43 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
-import { useLayout } from "@/contexts/LayoutContext"
-import { SidebarOpen } from "lucide-react"
+import { useState } from "react"
 
 const Write = () => {
-  const { sidebarOpen, setSidebarOpen } = useLayout()
+  const [modalOpen, setModalOpen] = useState(false)
+
+  const handleOpenModal = () => {
+    setModalOpen(true)
+  }
+  const handleCloseModal = () => {
+    setModalOpen(false)
+  }
+
+  if (modalOpen) {
+    return (
+      <div>
+        <p>Hello</p >
+        <button onClick={handleCloseModal}>Close</button>
+      </div >
+    )
+  }
 
   return (
-    <div className="pb-32 pt-6 md:pt-8 lg:pt-10">
-      <div className="w-17/20 max-w-4xl mx-auto flex flex-col gap-0.5 lg:gap-1">
-        <Input type="text" placeholder="Title" className="dark:bg-transparent py-2 border-none shadow-none text-3xl md:text-4xl lg:text-5xl h-auto font-heading" />
-        <Textarea placeholder="Tell your story..." className="dark:bg-transparent text-lg md:text-xl lg:text-2xl border-none shadow-none resize-none h-auto leading-relaxed" />
-      </div>
-      <div className={`fixed transition-all flex bottom-0 right-0 h-14 lg:h-18 border-t bg-background w-full items-center justify-center ${sidebarOpen ? `lg:w-[calc(100vw-240px)]` : ""}`}>
-        <div className="w-17/20 max-w-4xl flex justify-end">
-          <Button size="sm" className="px-6 lg:px-8 lg:h-10" onClick={() => alert("published")}>Publish</Button>
+    <div className="h-[calc(100dvh-56px)] relative">
+      <div className="h-[calc(100dvh-128px)] lg:h-[calc(100dvh-136px)] py-6 md:py-8 lg:py-10 overflow-y-auto">
+        <div className="w-17/20 max-w-4xl mx-auto flex flex-col gap-0.5 lg:gap-1.5">
+          <Input type="text" placeholder="Title" className="dark:bg-transparent py-2 border-none shadow-none text-3xl md:text-4xl lg:text-5xl h-auto font-heading" />
+          <Textarea placeholder="Tell your story..." className="dark:bg-transparent text-lg md:text-xl lg:text-2xl border-none shadow-none resize-none h-auto leading-relaxed" />
         </div>
       </div>
-    </div >
+      <div className="absolute flex bottom-0 right-0 h-14 lg:h-16 border-t bg-background w-full items-center justify-center">
+        <div className="w-17/20 max-w-4xl flex justify-end">
+          <Button variant="success" size="sm" className="px-8 lg:px-10 lg:h-9 rounded-full" onClick={handleOpenModal}>Publish</Button>
+        </div>
+      </div>
+    </div>
   )
+
 }
 
 export default Write
