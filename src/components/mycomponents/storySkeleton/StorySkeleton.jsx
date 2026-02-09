ede1d28dbaeaca86f9f8602ad7f12620ab48217a
Roshan-Roy <roshanroy2911@gmail.com>
2026-02-09 13:09:03 +0530

    scroll to top and story skeleton updated

--- src/components/mycomponents/storySkeleton/StorySkeleton.jsx
+++ src/components/mycomponents/storySkeleton/StorySkeleton.jsx
@@ -10,7 +10,7 @@ const StorySkeleton = () => {
                 </div>
                 <div className="pt-8 lg:pt-10 pb-30 flex flex-col gap-10 lg:gap-12">
                     {Array.from({ length: 10 }, (_, i) => (
-                        <div className="flex flex-col gap-2" key={i}>
+                        <div className="flex flex-col gap-2 lg:gap-3" key={i}>
                             <Skeleton className="h-4.5 md:h-5 lg:h-6 w-full" />
                             <Skeleton className="h-4.5 md:h-5 lg:h-6 w-full" />
                             <Skeleton className="h-4.5 md:h-5 lg:h-6 w-7/8" />
